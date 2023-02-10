e266a61e2d5de162aaa96bc1a101b43fff86caa7
didiidididididi <finpin2889@naver.com>
2023-02-10 16:55:40 +0900

    asdfdsfaf

--- project002/js/js.js
+++ project002/js/js.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
 
-    
+    // 오른쪽 화살표를 클릭했을때 메인슬라이드 텍스트와 이미지가 바뀌어라
+    $('.next_btn').click(function(){
+        let i = $(this).index()
+        $('.mainImg_img li').eq(i).addClass('on')
+    })
+
+
 })
