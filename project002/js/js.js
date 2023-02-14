7f1de00c9cffe4cb188f1cddf13ebe39632de954
didiidididididi <finpin2889@naver.com>
2023-02-14 16:51:02 +0900

    sadfsdafsdafsda

--- project002/js/js.js
+++ project002/js/js.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
 
     // 오른쪽 화살표를 클릭했을때 메인슬라이드 텍스트와 이미지가 바뀌어라
-    $('.next_btn').click(function(){
-        let i = $(this).index()
-        $('.mainImg_img li').eq(i).addClass('on')
-    })
 
+    let i = 0
+    $('.next_btn').click(function(){
+      if(i<=4) i++;
+      if(i==5) i=0;
+      console.log(i)
+        $('.mainImg_img li').removeClass('on')
+        $('.mainImg_txt>div').removeClass('on')
+
+
+        $('.mainImg_img li').eq(i).addClass('on')
+        $('.mainImg_txt>div').eq(i).addClass('on')
+    })
+    
+    $('.prev_btn').click(function(){
+        if(i>0) i--;
+        if(i==0) i=4;
+        console.log(i)
+        
+          $('.mainImg_img li').removeClass('on')
+          $('.mainImg_txt>div').removeClass('on')
+  
+  
+          $('.mainImg_img li').eq(i).addClass('on')
+          $('.mainImg_txt>div').eq(i).addClass('on')
+      })
+
+      // 장바구니 담기
+      let ii=0
+      $('.new_box21 li , .health_box21 li').click(function(){
+      ii++
+  
+          $('.shop span').text(ii)
+      })
+  
 
 })
