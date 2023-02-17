c333facb8762d07b3f2501916bdbb83bb2bfc202
didiidididididi <finpin2889@naver.com>
2023-02-17 16:06:05 +0900

    asdsadsadsada

--- project003/js/js.js
+++ project003/js/js.js
@@ -8,7 +8,30 @@ $(document).ready(function(){
         e.preventDefault();
         let i = $(this).index();
 
-        $('html,body').stop().animate({scrollTop:ht*i},800,'easeInBounce')
+    // 스타일시트 01번 적용스크립트 $('html,body').stop().animate({scrollTop:ht*i},800,'easeInBounce')
+    
+    $('section').removeClass('on')
+    $('section').eq(i).addClass('on')
+
+    })
+
+    let wd = $(window).width();
+    let hwd = $('header').width();
+
+    $('#wrap').width(wd-hwd)
+
+    $('#wrap').css({'left':hwd})
+
+    $(window).resize(function(){
+
+        let wd = $(window).width();
+        let hwd = $('header').width();
+
+    $('#wrap').width(wd-hwd)
+
+    $('wrap').css({'left':hwd})
+    $('section').height(ht);
+    
     })
 
 })
